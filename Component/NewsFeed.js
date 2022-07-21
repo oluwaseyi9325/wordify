07132e9a8fe1d5d2d9d547929a15ff380c66cd87
oluwaseyi9325 <adedokunseyi96@gmail.com>
2022-07-21 17:12:45 +0100

    evening

--- Component/NewsFeed.js
+++ Component/NewsFeed.js
@@ -10,12 +10,33 @@ function NewsFeed() {
      <div className='col-lg-3 col-sm-4 mt-3'>
      <div class="card shadow card-width">
     
-     <Image  class="card-img-top" src={instag} alt='card img' width={200} height={150} />
-     <div class="card-body">
+     <Image  class="card-img-top" src={instag} alt='card img' width={200} height={160} />
+     <div className='text-center text-muted  card-text mr-0' style={{marginTop:"-30px"}}>
+      
+     <section className='row'>
+       
+     <div className='col-5'>
+     <span className=' p-2' style={{backgroundColor:"whitesmoke"}}><i class="bi bi-hand-thumbs-up"></i> Like
+     <span className="badge badge-info">10</span></span>
+   </div>
+   <div className='col-7 '>
+
+    <span className=' p-2' style={{backgroundColor:"whitesmoke"}}>
+    <i class="bi bi-chat-left"></i> 
+    <span> Comment</span><span className="badge badge-info">10</span>
+    </span>
+ </div>
+  
+     </section>
+     
+     </div>
+    
+     <div class="card-body ">
        <h5 class="card-title">Card title</h5>
        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
        <a href="#" class="btn btn-primary">Go somewhere</a>
      </div>
+    
    </div>
      </div>
      <div className='col-lg-3 col-sm-4 mt-3 '>
